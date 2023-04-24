88bd1a4a00c5f0ce0b46df7bc846042be9469653
Andreas Ziegler <42463934+Andreas-Ziegler22@users.noreply.github.com>
2023-04-24 13:28:20 +0000

    first request using HTML

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.listen(3005, ()=>{
+app.use((req,res)=>{
+    // res.send('show me a ❤️!') // <- here we can see it works with HTML
+})
+
+app.listen(3001, ()=>{
     console.log('backend working...!')
 })
