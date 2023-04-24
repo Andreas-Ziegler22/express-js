aff728643cbc790637e2e82a38a232afb1f7487b
Andreas Ziegler <42463934+Andreas-Ziegler22@users.noreply.github.com>
2023-04-24 13:37:02 +0000

    fequest using JSON

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 
 app.use((req,res)=>{
+    // res.json({ 
+    //     name: 'Macbook Air M1',
+    //     price: 1199.00,
+    //     discount:0.00
+
+    // }) // <- here we can see it works JSON
+    
     // res.send('show me a ❤️!') // <- here we can see it works with HTML
 })
 
